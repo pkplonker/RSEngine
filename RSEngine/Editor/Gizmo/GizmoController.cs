9cc4a9bbc1c817193e4636f0e4a37e10679d62cd
Stuart H <pkplonker@gmail.com>
2025-10-21 22:05:49 +0100

    Gizmo Controls

--- RSEngine/Editor/Gizmo/GizmoController.cs
+++ RSEngine/Editor/Gizmo/GizmoController.cs
@@ -10,12 +10,13 @@ public class GizmoController
     private readonly IInputController inputController;
     private GizmoType currentGizmoType = GizmoType.None;
     
-    // Drag state
     private bool isDragging = false;
     private bool mouseIsDown = false;
     private GizmoAxis? dragAxis = null;
     private Vector3 dragStartPosition;
     private Vector3 objectStartPosition;
+    private Quaternion objectStartRotation;
+    private Vector3 objectStartScale;
     private Vector2 mouseStartPosition;
     private IRenderable? selectedObject;
     private ICamera? camera;
@@ -30,6 +31,30 @@ public class GizmoController
         selectionManager.SelectionChanged += OnSelectionChanged;
         inputController.MouseMove += OnMouseMove;
         inputController.SubscribeToMouseButtonEvent(OnMouseButton);
+        inputController.SubscribeToKeyEvent(OnKeyPress);
+    }
+
+    private bool OnKeyPress(IInputController.Key key, IInputController.InputState state)
+    {
+        if (selectedObject == null) return false;
+        if (state != IInputController.InputState.Pressed) return false;
+
+        GizmoType newType = key switch
+        {
+            IInputController.Key.W => GizmoType.Translate,
+            IInputController.Key.E => GizmoType.Rotate,
+            IInputController.Key.R => GizmoType.Scale,
+            _ => currentGizmoType
+        };
+
+        if (newType != currentGizmoType)
+        {
+            currentGizmoType = newType;
+            gizmoScene.SetGizmo(currentGizmoType, selectedObject.ModelMatrix);
+            Logger.Log($"Switched to {currentGizmoType} gizmo");
+        }
+
+        return false;
     }
 
     private bool OnMouseButton(IInputController.MouseButton button, IInputController.InputState state)
@@ -40,8 +65,6 @@ public class GizmoController
         {
             mouseIsDown = true;
             mouseStartPosition = inputController.GetMousePosition();
-            
-            //Logger.Log($"Mouse down at {mouseStartPosition}");
         }
         else if (state == IInputController.InputState.Released)
         {
@@ -49,7 +72,7 @@ public class GizmoController
             
             if (isDragging)
             {
-                //Logger.Log($"Stopped dragging");
+                Logger.Log($"Finished {currentGizmoType} operation on {dragAxis} axis");
             }
             isDragging = false;
             dragAxis = null;
@@ -66,15 +89,84 @@ public class GizmoController
         Vector2 currentMousePos = new Vector2(x, y);
         Vector2 mouseDelta = currentMousePos - mouseStartPosition;
 
-        //Logger.Log($"Mouse delta: {mouseDelta}, dragging {dragAxis} axis");
+        switch (currentGizmoType)
+        {
+            case GizmoType.Translate:
+                HandleTranslate(mouseDelta);
+                break;
+            case GizmoType.Rotate:
+                HandleRotate(mouseDelta);
+                break;
+            case GizmoType.Scale:
+                HandleScale(mouseDelta);
+                break;
+        }
+        
+        gizmoScene.SetGizmo(currentGizmoType, selectedObject.ModelMatrix);
+    }
+
+    private void HandleTranslate(Vector2 mouseDelta)
+    {
+        if (selectedObject == null || dragAxis == null) return;
 
         Vector3 movement = CalculateAxisMovement(mouseDelta, dragAxis.Value);
-        
         Vector3 newPosition = objectStartPosition + movement;
 
         UpdateObjectPosition(selectedObject, newPosition);
+    }
+
+   private void HandleRotate(Vector2 mouseDelta)
+    {
+        if (selectedObject == null || dragAxis == null || !(selectedObject is Component component)) 
+            return;
+
+        float rotationAmount = (mouseDelta.X + mouseDelta.Y) * 0.5f;
+        float rotationRadians = rotationAmount * 0.01f;
+
+        if (dragAxis.Value == GizmoAxis.Z)
+        {
+            rotationRadians = -rotationRadians;
+        }
+
+        Vector3 rotationAxis = dragAxis.Value switch
+        {
+            GizmoAxis.X => Vector3.UnitX,
+            GizmoAxis.Y => Vector3.UnitY,
+            GizmoAxis.Z => Vector3.UnitZ,
+            _ => Vector3.UnitY
+        };
+
+        Quaternion deltaRotation = Quaternion.CreateFromAxisAngle(rotationAxis, rotationRadians);
+        Quaternion newRotation = objectStartRotation * deltaRotation;
+
+        component.GameObject.Transform.Rotation = newRotation;
+    }
+
+    private void HandleScale(Vector2 mouseDelta)
+    {
+        if (selectedObject == null || dragAxis == null || !(selectedObject is Component component)) 
+            return;
+
+        float scaleDelta = (mouseDelta.X - mouseDelta.Y) * 0.001f;
         
-        gizmoScene.SetGizmo(currentGizmoType, selectedObject.ModelMatrix);
+        if (dragAxis.Value == GizmoAxis.Z)
+        {
+            scaleDelta = -scaleDelta;
+        }
+        
+        float scaleFactor = 1.0f + scaleDelta;
+
+        Vector3 newScale = dragAxis.Value switch
+        {
+            GizmoAxis.X => objectStartScale * new Vector3(scaleFactor, 1, 1),
+            GizmoAxis.Y => objectStartScale * new Vector3(1, scaleFactor, 1),
+            GizmoAxis.Z => objectStartScale * new Vector3(1, 1, scaleFactor),
+            _ => objectStartScale * scaleFactor
+        };
+
+        newScale = Vector3.Max(newScale, new Vector3(0.001f, 0.001f, 0.001f));
+
+        component.GameObject.Transform.Scale = newScale;
     }
 
     private Vector3 CalculateAxisMovement(Vector2 mouseDelta, GizmoAxis axis)
@@ -124,14 +216,32 @@ public class GizmoController
         return new Vector3(matrix.M41, matrix.M42, matrix.M43);
     }
 
+    private Quaternion GetObjectRotation(IRenderable obj)
+    {
+        if (obj is Component component)
+        {
+            return component.GameObject.Transform.Rotation;
+        }
+        
+        Matrix4x4 matrix = obj.ModelMatrix;
+        Matrix4x4.Decompose(matrix, out _, out Quaternion rotation, out _);
+        return rotation;
+    }
+
+    private Vector3 GetObjectScale(IRenderable obj)
+    {
+        if (obj is Component component)
+        {
+            return component.GameObject.Transform.Scale;
+        }
+        
+        Matrix4x4 matrix = obj.ModelMatrix;
+        Matrix4x4.Decompose(matrix, out Vector3 scale, out _, out _);
+        return scale;
+    }
+
     private void UpdateObjectPosition(IRenderable obj, Vector3 newPosition)
     {
-        Matrix4x4 currentMatrix = obj.ModelMatrix;
-        
-        currentMatrix.M41 = newPosition.X;
-        currentMatrix.M42 = newPosition.Y;
-        currentMatrix.M43 = newPosition.Z;
-        
         if (obj is Component component)
         {
            component.GameObject.Transform.Position = newPosition;
@@ -140,7 +250,6 @@ public class GizmoController
         {
             Logger.Log("UpdateObjectPosition failed as object is not a component");
         }
-        
     }
 
     private void OnSelectionChanged(IRenderable? obj)
@@ -150,12 +259,15 @@ public class GizmoController
             isDragging = true;
             dragAxis = gizmoAxis.Axis;
             objectStartPosition = GetObjectPosition(selectedObject);
+            objectStartRotation = GetObjectRotation(selectedObject);
+            objectStartScale = GetObjectScale(selectedObject);
             dragStartPosition = objectStartPosition;
             
-            //Logger.Log($"Started dragging on {gizmoAxis.Axis} axis");
+            Logger.Log($"Started {currentGizmoType} on {gizmoAxis.Axis} axis");
             return;
         }
         
+        // Clear selection
         if (obj == null)
         {
             selectedObject = null;
@@ -171,8 +283,6 @@ public class GizmoController
         isDragging = false;
         dragAxis = null;
         
-       
-
         selectedObject = obj;
         
         if (currentGizmoType == GizmoType.None)
--- RSEngine/Editor/Gizmo/GizmoScene.cs
+++ RSEngine/Editor/Gizmo/GizmoScene.cs
@@ -1,7 +1,6 @@
 ﻿using System.Numerics;
 using Engine;
 using Silk.NET.OpenGL;
-using Shader = Silk.NET.OpenGL.Shader;
 
 namespace Editor;
 
@@ -11,24 +10,20 @@ public class GizmoScene : IScene
 
     private IShader? gizmoShader;
     private IShader? pickingShader;
-    private uint vao, vbo, ebo;
-    private bool initialized = false;
-    private uint indexCount = 0;
-    public float ArrowThickness { get; set; } = 0.1f;
+    private Matrix4x4 modelMatrix;
+    private GizmoController.GizmoType currentGizmoType = GizmoController.GizmoType.None;
+    
+    // Different gizmo implementations
+    private TranslationGizmo? translationGizmo;
+    private RotationGizmo? rotationGizmo;
+    private ScaleGizmo? scaleGizmo;
+    
+    private static readonly GizmoSceneRenderer renderer = new GizmoSceneRenderer();
 
     // Picking IDs for each axis
     private static readonly RenderID24 X_AXIS_ID = new RenderID24(1);
     private static readonly RenderID24 Y_AXIS_ID = new RenderID24(2);
     private static readonly RenderID24 Z_AXIS_ID = new RenderID24(3);
-
-    // Store index ranges for each arrow
-    private (uint start, uint count) xAxisIndices;
-    private (uint start, uint count) yAxisIndices;
-    private (uint start, uint count) zAxisIndices;
-    private Matrix4x4 modelMatrix;
-    private GizmoController.GizmoType currentGizmoType = GizmoController.GizmoType.None;
-    
-    private static readonly GizmoSceneRenderer renderer = new GizmoSceneRenderer();
 
     public GizmoScene()
     {
@@ -98,254 +93,68 @@ public class GizmoScene : IScene
         }
     }
 
-    public unsafe void Initialize(GL gl)
+    public void Initialize(GL gl)
     {
-        if (initialized) return;
-
-        List<float> vertices = new List<float>();
-        List<uint> indices = new List<uint>();
-
-        float arrowLength = 1.0f;
-        float arrowHeadLength = 0.2f;
-        float arrowHeadRadius = ArrowThickness;
-        int coneSegments = 12;
-
-        uint currentIndex = 0;
-        uint startIndex = 0;
-
-        // X-Axis (Red)
-        startIndex = (uint)indices.Count;
-        AddArrow(vertices, indices, ref currentIndex,
-            new Vector3(0, 0, 0), new Vector3(arrowLength, 0, 0),
-            new Vector3(1, 0, 0),
-            arrowHeadLength, arrowHeadRadius, coneSegments);
-        xAxisIndices = (startIndex, (uint)indices.Count - startIndex);
-
-        // Y-Axis (Green)
-        startIndex = (uint)indices.Count;
-        AddArrow(vertices, indices, ref currentIndex,
-            new Vector3(0, 0, 0), new Vector3(0, arrowLength, 0),
-            new Vector3(0, 1, 0),
-            arrowHeadLength, arrowHeadRadius, coneSegments);
-        yAxisIndices = (startIndex, (uint)indices.Count - startIndex);
-
-        // Z-Axis (Blue)
-        startIndex = (uint)indices.Count;
-        AddArrow(vertices, indices, ref currentIndex,
-            new Vector3(0, 0, 0), new Vector3(0, 0, arrowLength),
-            new Vector3(0, 0, 1),
-            arrowHeadLength, arrowHeadRadius, coneSegments);
-        zAxisIndices = (startIndex, (uint)indices.Count - startIndex);
-
-        float[] quadVertices = vertices.ToArray();
-        uint[] quadIndices = indices.ToArray();
-        indexCount = (uint)quadIndices.Length;
-
-        gl.GenVertexArrays(1, out vao);
-        gl.GenBuffers(1, out vbo);
-        gl.GenBuffers(1, out ebo);
-
-        gl.BindVertexArray(vao);
-
-        gl.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
-        fixed (float* vertPtr = quadVertices)
-        {
-            gl.BufferData(BufferTargetARB.ArrayBuffer,
-                (nuint)(quadVertices.Length * sizeof(float)),
-                vertPtr, BufferUsageARB.StaticDraw);
-        }
-
-        gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, ebo);
-        fixed (uint* indPtr = quadIndices)
-        {
-            gl.BufferData(BufferTargetARB.ElementArrayBuffer,
-                (nuint)(quadIndices.Length * sizeof(uint)),
-                indPtr, BufferUsageARB.StaticDraw);
-        }
-
-        gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false,
-            6 * sizeof(float), (void*)0);
-        gl.EnableVertexAttribArray(0);
-
-        gl.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false,
-            6 * sizeof(float), (void*)(3 * sizeof(float)));
-        gl.EnableVertexAttribArray(1);
-
-        gl.BindVertexArray(0);
-        initialized = true;
-    }
-
-    private void AddArrow(List<float> vertices, List<uint> indices, ref uint currentIndex,
-        Vector3 start, Vector3 end, Vector3 color,
-        float headLength, float headRadius, int segments)
-    {
-        Vector3 direction = Vector3.Normalize(end - start);
-        float shaftLength = Vector3.Distance(start, end) - headLength;
-        Vector3 shaftEnd = start + direction * shaftLength;
-
-        float shaftRadius = headRadius * 0.3f;
-
-        Vector3 perpendicular1 = Vector3.Normalize(Vector3.Cross(direction,
-            Math.Abs(direction.Y) > 0.9f ? new Vector3(1, 0, 0) : new Vector3(0, 1, 0)));
-        Vector3 perpendicular2 = Vector3.Normalize(Vector3.Cross(direction, perpendicular1));
-
-        // ===== Arrow Shaft (Cylinder) =====
-        uint shaftStartIndex = currentIndex;
-
-        for (int i = 0; i < segments; i++)
-        {
-            float angle = (i / (float)segments) * MathF.PI * 2.0f;
-            Vector3 offset = (perpendicular1 * MathF.Cos(angle) + perpendicular2 * MathF.Sin(angle)) * shaftRadius;
-            Vector3 point = start + offset;
-            vertices.AddRange(new[] { point.X, point.Y, point.Z, color.X, color.Y, color.Z });
-            currentIndex++;
-        }
-
-        uint shaftEndIndex = currentIndex;
-        for (int i = 0; i < segments; i++)
-        {
-            float angle = (i / (float)segments) * MathF.PI * 2.0f;
-            Vector3 offset = (perpendicular1 * MathF.Cos(angle) + perpendicular2 * MathF.Sin(angle)) * shaftRadius;
-            Vector3 point = shaftEnd + offset;
-            vertices.AddRange(new[] { point.X, point.Y, point.Z, color.X, color.Y, color.Z });
-            currentIndex++;
-        }
-
-        for (int i = 0; i < segments; i++)
-        {
-            int next = (i + 1) % segments;
-
-            indices.Add(shaftStartIndex + (uint)i);
-            indices.Add(shaftEndIndex + (uint)i);
-            indices.Add(shaftStartIndex + (uint)next);
-
-            indices.Add(shaftStartIndex + (uint)next);
-            indices.Add(shaftEndIndex + (uint)i);
-            indices.Add(shaftEndIndex + (uint)next);
-        }
-
-        // ===== Arrow Head (Cone) =====
-        Vector3 coneTip = end;
-
-        uint tipIndex = currentIndex;
-        vertices.AddRange(new[] { coneTip.X, coneTip.Y, coneTip.Z, color.X, color.Y, color.Z });
-        currentIndex++;
-
-        uint coneBaseIndex = currentIndex;
-        for (int i = 0; i < segments; i++)
-        {
-            float angle = (i / (float)segments) * MathF.PI * 2.0f;
-            Vector3 offset = (perpendicular1 * MathF.Cos(angle) + perpendicular2 * MathF.Sin(angle)) * headRadius;
-            Vector3 point = shaftEnd + offset;
-            vertices.AddRange(new[] { point.X, point.Y, point.Z, color.X, color.Y, color.Z });
-            currentIndex++;
-        }
-
-        for (int i = 0; i < segments; i++)
-        {
-            int next = (i + 1) % segments;
-
-            indices.Add(tipIndex);
-            indices.Add(coneBaseIndex + (uint)i);
-            indices.Add(coneBaseIndex + (uint)next);
-        }
-
-        Vector3 baseCenter = shaftEnd;
-        uint baseCenterIndex = currentIndex;
-        vertices.AddRange(new[] { baseCenter.X, baseCenter.Y, baseCenter.Z, color.X, color.Y, color.Z });
-        currentIndex++;
-
-        for (int i = 0; i < segments; i++)
-        {
-            int next = (i + 1) % segments;
-
-            indices.Add(baseCenterIndex);
-            indices.Add(coneBaseIndex + (uint)next);
-            indices.Add(coneBaseIndex + (uint)i);
-        }
+        translationGizmo ??= new TranslationGizmo();
+        rotationGizmo ??= new RotationGizmo();
+        scaleGizmo ??= new ScaleGizmo();
+        
+        translationGizmo.Initialize(gl);
+        rotationGizmo.Initialize(gl);
+        scaleGizmo.Initialize(gl);
     }
 
     internal unsafe void RenderGeometry(IRenderer renderer, RenderPassData data, IRenderPass renderPass)
     {
         if (currentGizmoType == GizmoController.GizmoType.None) return;
 
-        if (!initialized)
+        if (translationGizmo == null || rotationGizmo == null || scaleGizmo == null)
             Initialize(renderer.Gl);
 
-        renderer.Gl.BindVertexArray(vao);
-
-        if (renderPass?.TargetType == RenderTargetType.Picking)
+        GizmoBase? activeGizmo = currentGizmoType switch
         {
-            // Picking pass - render each axis with a unique ID
-            if (PickingShader == null) return;
+            GizmoController.GizmoType.Translate => translationGizmo,
+            GizmoController.GizmoType.Rotate => rotationGizmo,
+            GizmoController.GizmoType.Scale => scaleGizmo,
+            _ => null
+        };
 
-            renderer.UseShader(PickingShader);
-            PickingShader.SetUniform("uView", data.View);
-            PickingShader.SetUniform("uProjection", data.Projection);
-            PickingShader.SetUniform("uModel", modelMatrix);
+        if (activeGizmo == null) return;
 
-            // X-Axis
-            SetPickingColor(PickingShader, X_AXIS_ID, SceneID);
-            renderer.Gl.DrawElements(Silk.NET.OpenGL.PrimitiveType.Triangles,
-                xAxisIndices.count,
-                DrawElementsType.UnsignedInt,
-                (void*)(xAxisIndices.start * sizeof(uint)));
+        bool isPicking = renderPass?.TargetType == RenderTargetType.Picking;
+        IShader? shader = isPicking ? PickingShader : GizmoShader;
+        
+        if (shader == null) return;
 
-            // Y-Axis
-            SetPickingColor(PickingShader, Y_AXIS_ID, SceneID);
-            renderer.Gl.DrawElements(Silk.NET.OpenGL.PrimitiveType.Triangles,
-                yAxisIndices.count,
-                DrawElementsType.UnsignedInt,
-                (void*)(yAxisIndices.start * sizeof(uint)));
+        renderer.UseShader(shader);
 
-            // Z-Axis
-            SetPickingColor(PickingShader, Z_AXIS_ID, SceneID);
-            renderer.Gl.DrawElements(Silk.NET.OpenGL.PrimitiveType.Triangles,
-                zAxisIndices.count,
-                DrawElementsType.UnsignedInt,
-                (void*)(zAxisIndices.start * sizeof(uint)));
-        }
-        else
+        if (!isPicking)
         {
-            // Normal rendering - render all at once with colors
-            if (GizmoShader == null) return;
-
             renderer.Gl.Disable(EnableCap.DepthTest);
             renderer.Gl.Enable(EnableCap.Blend);
             renderer.Gl.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+        }
 
-            renderer.UseShader(GizmoShader);
-            GizmoShader.SetUniform("uView", data.View);
-            GizmoShader.SetUniform("uProjection", data.Projection);
-            GizmoShader.SetUniform("uModel", modelMatrix);
+        activeGizmo.Render(renderer.Gl, shader, modelMatrix, data.View, data.Projection, isPicking, SceneID);
 
-            renderer.Gl.DrawElements(Silk.NET.OpenGL.PrimitiveType.Triangles, indexCount,
-                DrawElementsType.UnsignedInt, null);
-
+        if (!isPicking)
+        {
             renderer.Gl.Enable(EnableCap.DepthTest);
             renderer.Gl.Disable(EnableCap.Blend);
         }
     }
 
-    private void SetPickingColor(IShader pickingShader, RenderID24 renderID, byte sceneID)
+    public void SetGizmo(GizmoController.GizmoType gizmoType, Matrix4x4 objModelMatrix)
     {
-        uint objectId = renderID.Value;
-
-        // Pack scene ID and object ID the same way as GameObjects
-        uint packedId = ((uint)sceneID << 24) | objectId;
-
-        float r = (packedId & 0xFF) / 255.0f;
-        float g = ((packedId >> 8) & 0xFF) / 255.0f;
-        float b = ((packedId >> 16) & 0xFF) / 255.0f;
-        float a = ((packedId >> 24) & 0xFF) / 255.0f;
-
-        pickingShader.SetUniform("uObjectColor", new Vector4(r, g, b, a));
-    }
-
-    public void SetGizmo(GizmoController.GizmoType currentGizmoType, Matrix4x4 objModelMatrix)
-    {
-        this.currentGizmoType = currentGizmoType;
+        this.currentGizmoType = gizmoType;
         modelMatrix = objModelMatrix;
+    }
+    
+    public void Dispose(GL gl)
+    {
+        translationGizmo?.Dispose(gl);
+        rotationGizmo?.Dispose(gl);
+        scaleGizmo?.Dispose(gl);
     }
 }
 
@@ -354,41 +163,4 @@ public enum GizmoAxis
     X,
     Y,
     Z
-}
-
-public class GizmoAxisRenderable : IGizmoRenderable
-{
-    private readonly GizmoAxis axis;
-
-    public GizmoAxisRenderable(GizmoAxis axis)
-    {
-        this.axis = axis;
-    }
-
-    public GizmoAxis Axis => axis;
-
-    public void Render(IRenderer renderer, RenderPassData data, CustomShaderArgs customShaderArgs = null)
-    {
-        // This is just a marker for selection, actual rendering is done by GizmoScene
-    }
-
-    public RenderID24 RenderID
-    {
-        get => axis switch
-        {
-            GizmoAxis.X => new RenderID24(1),
-            GizmoAxis.Y => new RenderID24(2),
-            GizmoAxis.Z => new RenderID24(3),
-            _ => new RenderID24(0)
-        };
-        set { }
-    }
-
-    public IShader? Shader => null;
-    public Matrix4x4 ModelMatrix => Matrix4x4.Identity;
-
-    public override string ToString()
-    {
-        return $"Gizmo {axis}-Axis";
-    }
 }
